b34a816e54ae42b3dab63cd389f5fc0e6940f3f8
Shawn Allen <shawn.allen@gsa.gov>
2016-01-15 14:32:47 -0800

    add jsdoc comment to property.js

--- src/property.js
+++ src/property.js
@@ -1,5 +1,26 @@
 var privates = require('./privates');
 
+/**
+ * A helpful property descriptor generator. As in:
+ *
+ * @example
+ * var change = function(value, previous) {
+ *   console.log('foo changed from', previous, 'to', value);
+ * };
+ *
+ * Object.defineProperty(proto, 'foo', change);
+ *
+ * @param {String}    name    The *private* property name. This need not be the
+ *                            same as the property's public name.
+ * @param {Function}  change  The function to call when the value changes. If
+ *                            this function returns `false` the change will be
+ *                            canceled.
+ * @param {Function?} parse   An optional function to parse or otherwise coerce
+ *                            values, e.g. to turn strings into numbers.
+ *
+ * @return {Object}   returns a property descriptor for use with
+ *                    Object.defineProperty()
+ */
 module.exports = function property(name, change, parse) {
   return {
     get: function() {
