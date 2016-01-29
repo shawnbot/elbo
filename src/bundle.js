beedcbc7165ccb357785ab9a7f70d6f4ac94f062
Shawn Allen <shawn.allen@gsa.gov>
2016-01-29 15:34:57 -0800

    blow away old stuff

--- src/bundle.js
+++
@@ -1,5 +0,0 @@
-// include the document.registerElement() polyfill
-require('document-register-element');
-
-// make the constructor available as a global variable
-window.CustomElement = require('./element');
